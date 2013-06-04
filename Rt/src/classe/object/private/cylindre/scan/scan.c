8e0c177bdb698976388dbca81d3c36f647a3649f
ColliotV <collio_v@epitech.eu>
2013-06-04 15:38:35 +0200

    argggg

--- Rt/src/classe/object/private/cylindre/scan/scan.c
+++ Rt/src/classe/object/private/cylindre/scan/scan.c
@@ -5,7 +5,7 @@
 ** Login   <collio_v@epitech.net>
 **
 ** Started on  Fri May 31 15:17:18 2013 vincent colliot
-** Last update Tue Jun  4 15:14:18 2013 vincent colliot
+** Last update Tue Jun  4 15:29:59 2013 vincent colliot
 */
 
 #include <stdio.h>
@@ -54,19 +54,19 @@ void    cylindre_focus(void *l, t_token *token)
     def_error("x");
   if (!O_IN(s + strspn(s, "-+"), "0123456789."))
     def_error("x");
-  ((t_cylindre*)l)->focus.x = (N(atof(s)) * M_PI) / 180;
+  ((t_cylindre*)l)->focus.x = (N(atof(s + strspn(s, "-+"))) * M_PI) / 180;
   free(s);
   if ((s = xml_token(&token, "z", RESOLVE, 0)) == NULL)
     def_error("z");
   if (!O_IN(s + strspn(s, "-+"), "0123456789."))
     def_error("z");
-  ((t_cylindre*)l)->focus.z = (N(atof(s)) * M_PI) / 180;
+  ((t_cylindre*)l)->focus.z = (N(atof(s + strspn(s, "-+"))) * M_PI) / 180;
   free(s);
   if ((s = xml_token(&token, "y", RESOLVE, 0)) == NULL)
     def_error("y");
   if (!O_IN(s + strspn(s, "-+"), "0123456789."))
     def_error("y");
-  ((t_cylindre*)l)->focus.y = (N(atof(s)) * M_PI) / 180;
+  ((t_cylindre*)l)->focus.y = (N(atof(s + strspn(s, "-+"))) * M_PI) / 180;
   free(s);
   ((t_cylindre*)l)->focus = convert_focus(((t_cylindre*)l)->focus);
 }
