d87e4802e6d91bb446cd66a4813fb3023517b4c1
my-get-dodo <thomas.lecorre@epitech.eu>
2013-06-04 21:26:28 +0200

    filtre negatif

--- Rt/src/screen_load/filtre.c
+++ Rt/src/screen_load/filtre.c
@@ -5,13 +5,16 @@
 ** Login   <lecorr_b@epitech.net>
 **
 ** Started on  Tue Jun  4 15:35:59 2013 thomas lecorre
-** Last update Tue Jun  4 20:35:14 2013 thomas lecorre
+** Last update Tue Jun  4 21:26:15 2013 thomas lecorre
 */
 
 #include "u_color.h"
 
 t_color		filtre(t_color pix)
 {
+
+  /* NOIR ET BLANC */
+
   /* float	stock; */
 
   /* stock = 0.0; */
@@ -21,7 +24,18 @@ t_color		filtre(t_color pix)
   /* (pix.rgb)[R] = stock / 3; */
   /* (pix.rgb)[G] = stock / 3; */
   /* (pix.rgb)[B] = stock / 3; */
-  (pix.rgb)[R] += 25;
-  (pix.rgb)[G] += 25;
+
+  /* SEPIA */
+
+  /* (pix.rgb)[R] += 35; */
+  /* (pix.rgb)[G] += 35; */
+
+
+  /* NEGATIF */
+
+  /* (pix.rgb)[R] = 255 - (pix.rgb)[R]; */
+  /* (pix.rgb)[G] = 255 - (pix.rgb)[G]; */
+  /* (pix.rgb)[B] = 255 - (pix.rgb)[B]; */
+
   return (pix);
 }
